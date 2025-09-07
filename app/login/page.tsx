35ca8278a794430831ce557110fd58c4f6f48727
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2025-09-07 12:14:45 +0900

    add: export default 추가

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+
+import LoginForm from "./_components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 80 }}>
+      <LoginForm />
+    </main>
+  );
+}
